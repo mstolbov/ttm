be8408a202ca86ddc959ac60416701156d2dd741
Mike Stolbov <mstolbov@gmail.com>
2016-01-27 04:43:27 +0300

    test show task on welcome page

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+  setup do
+    @tasks = create_list :task, 15
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+    assert true, assigns(:tasks).present?
   end
 
 end
